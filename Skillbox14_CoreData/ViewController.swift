3ecffe295dcf05187ee421a269a60b2c4354d4c0
NanoMuffin <nanomuffin@yandex.ru>
2020-05-03 04:30:39 +0800

    Initial Commit

---
+++ Skillbox14_CoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Skillbox14_CoreData
+//
+//  Created by Александр Сорока on 03.05.2020.
+//  Copyright © 2020 Александр Сорока. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
